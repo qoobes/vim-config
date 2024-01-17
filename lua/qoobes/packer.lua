29b85fd55ca7b04f96703112c283e77142b6de6c
Ahmed Haracic <me@qoobes.dev>
2024-01-17 21:45:13 +0100

    Reformat and add a few new plugins

--- lua/qoobes/packer.lua
+++ lua/qoobes/packer.lua
@@ -1,71 +1,83 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
 	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+	use("wbthomason/packer.nvim")
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
 		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-  use({ 'rose-pine/neovim', as = 'rose-pine',
-    config = function()
-      vim.cmd('colorscheme rose-pine')
-      end
-  })
+	use({
+		"rose-pine/neovim",
+		as = "rose-pine",
+		config = function()
+			vim.cmd("colorscheme rose-pine")
+		end,
+	})
 
-  -- use 'rebelot/kanagawa.nvim'
+	-- use 'rebelot/kanagawa.nvim'
 
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-	use('theprimeagen/harpoon')
+	use("theprimeagen/harpoon")
 
-	use('tpope/vim-fugitive')
+	use("tpope/vim-fugitive")
 
-  use('terrortylor/nvim-comment')
+	use("terrortylor/nvim-comment")
 
-
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
 		requires = {
 			--- Uncomment these if you want to manage LSP servers from neovim
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
-      {'jose-elias-alvarez/null-ls.nvim'},
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+			{ "jose-elias-alvarez/null-ls.nvim" },
 			-- LSP Support
-			{'neovim/nvim-lspconfig'},
+			{ "neovim/nvim-lspconfig" },
 			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
-		}
-	}
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "L3MON4D3/LuaSnip" },
+		},
+	})
 
 	-- use { "zbirenbaum/copilot.lua", }
 
 	use("onsails/lspkind.nvim")
-  use("nvim-lualine/lualine.nvim")
+	use("nvim-lualine/lualine.nvim")
 
-  use {
-    "nvim-telescope/telescope-file-browser.nvim",
-    requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-  }
+	use({
+		"nvim-telescope/telescope-file-browser.nvim",
+		requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	})
 
-  use 'nvim-tree/nvim-web-devicons'
+	use("nvim-tree/nvim-web-devicons")
 
-  use {
-    "windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup {} end
-  }
-  use 'windwp/nvim-ts-autotag'
-  use 'fsouza/prettierd'
-  use 'mhartington/formatter.nvim'
-  use 'stevearc/dressing.nvim'
-  use 'MunifTanjim/nui.nvim'
-  use 'folke/noice.nvim'
-  use 'tpope/vim-surround'
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
+	use("windwp/nvim-ts-autotag")
+	use("fsouza/prettierd")
+	use("stevearc/dressing.nvim")
+	use("MunifTanjim/nui.nvim")
+	use("folke/noice.nvim")
+	use("tpope/vim-surround")
+
+	use({
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup()
+		end,
+	})
+
+	use("mattn/emmet-vim")
 end)
