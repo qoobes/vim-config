a05cd7c1ecf5471010037a3e8f4c7204be08ffd5
Ahmed Haracic <me@qoobes.dev>
2024-01-17 23:45:48 +0100

    Fix telescope config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,38 +1,39 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>pv",
-  ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-  { noremap = true }
-)
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+-- vim.api.nvim_set_keymap(
+-- 	"n",
+-- 	"<leader>pv",
+-- 	":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+-- 	{ noremap = true }
+-- )
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 -- local fb_actions = require "telescope".extensions.file_browser.actions
 
-require("telescope").setup {
-  initial_mode = "normal",
-  extensions = {
-    file_browser = {
-      initial_mode = "normal",
-      theme = "ivy",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = false,
-      -- mappings = {
-      --   ["i"] = {
-      --     -- your custom insert mode mappings
-      --   },
-      --   ["n"] = {
-      --     ["<leader>ff"] = fb_actions.toggle_browser,
-      --     ["<leader>nn"] = fb_actions.create
-      --   },
-      -- },
-    },
-  },
-}
+require("telescope").setup({
+	initial_mode = "normal",
+	extensions = {
+		-- file_browser = {
+		-- 	enabled = false,
+		-- initial_mode = "normal",
+		-- theme = "ivy",
+		-- disables netrw and use telescope-file-browser in its place
+		-- hijack_netrw = false,
+		-- mappings = {
+		--   ["i"] = {
+		--     -- your custom insert mode mappings
+		--   },
+		--   ["n"] = {
+		--     ["<leader>ff"] = fb_actions.toggle_browser,
+		--     ["<leader>nn"] = fb_actions.create
+		--   },
+		-- },
+		-- },
+	},
+})
 
-require("telescope").load_extension "file_browser"
+require("telescope").load_extension("file_browser")
