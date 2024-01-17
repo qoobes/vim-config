a3b569aab879b7a6421cfff12c79bae5d068d6f2
Ahmed Haracic <me@qoobes.dev>
2024-01-17 23:46:04 +0100

    Comment out prettier config

--- after/plugin/prettier.lua
+++ after/plugin/prettier.lua
@@ -1,48 +1,48 @@
-local withPrettierd = function()
-  return {
-    exe = "prettierd",
-    args = {vim.api.nvim_buf_get_name(0)},
-    stdin = true
-  }
-end
-
-require('formatter').setup({
-  logging = true,
-  log_level = vim.log.levels.WARN,
-
-  filetype = {
-    javascript = withPrettierd,
-    typescript = withPrettierd,
-    javascriptreact = withPrettierd,
-    typescriptreact = withPrettierd,
-  },
-})
-
-local lspconfig = require('lspconfig')
-
-local function organize_imports()
-  local params = {
-    command = "_typescript.organizeImports",
-    arguments = {vim.api.nvim_buf_get_name(0)},
-    title = ""
-  }
-  vim.lsp.buf.execute_command(params)
-end
-
-lspconfig.tsserver.setup {
-  commands = {
-    OrganizeImports = {
-      organize_imports,
-      description = "Organize Imports"
-    }
-  }
-}
-
--- Format and organise on save 
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-augroup("__formatter__", { clear = true })
-autocmd("BufWritePost", {
-	group = "__formatter__",
-	command = ":FormatWrite",
-})
+-- local withPrettierd = function()
+--   return {
+--     exe = "prettierd",
+--     args = {vim.api.nvim_buf_get_name(0)},
+--     stdin = true
+--   }
+-- end
+--
+-- require('formatter').setup({
+--   logging = true,
+--   log_level = vim.log.levels.WARN,
+--
+--   filetype = {
+--     javascript = withPrettierd,
+--     typescript = withPrettierd,
+--     javascriptreact = withPrettierd,
+--     typescriptreact = withPrettierd,
+--   },
+-- })
+--
+-- local lspconfig = require('lspconfig')
+--
+-- local function organize_imports()
+--   local params = {
+--     command = "_typescript.organizeImports",
+--     arguments = {vim.api.nvim_buf_get_name(0)},
+--     title = ""
+--   }
+--   vim.lsp.buf.execute_command(params)
+-- end
+--
+-- lspconfig.tsserver.setup {
+--   commands = {
+--     OrganizeImports = {
+--       organize_imports,
+--       description = "Organize Imports"
+--     }
+--   }
+-- }
+--
+-- -- Format and organise on save 
+-- local augroup = vim.api.nvim_create_augroup
+-- local autocmd = vim.api.nvim_create_autocmd
+-- augroup("__formatter__", { clear = true })
+-- autocmd("BufWritePost", {
+-- 	group = "__formatter__",
+-- 	command = ":FormatWrite",
+-- })
