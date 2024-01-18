6723980cf63c2181477c78780224b51feb888c0f
Ahmed Haracic <me@qoobes.dev>
2024-01-18 20:31:24 +0100

    Give commands proper names

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,34 +4,47 @@ local lsp_zero = require("lsp-zero")
 lsp_zero.on_attach(function(client, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 
+	local function getOpts(text)
+		return {
+			buffer = bufnr,
+			remap = false,
+			desc = text,
+			noremap = true,
+		}
+	end
+
 	vim.keymap.set("n", "gd", function()
 		vim.lsp.buf.definition()
-	end, opts)
+	end, getOpts("Go to definition"))
+
 	vim.keymap.set("n", "K", function()
 		vim.lsp.buf.hover()
-	end, opts)
+	end, getOpts("Hover to see available options."))
+
 	vim.keymap.set("n", "<leader>vws", function()
 		vim.lsp.buf.workspace_symbol()
-	end, opts)
+	end, getOpts("Search workspace symbols"))
+
 	vim.keymap.set("n", "<leader>vd", function()
 		vim.diagnostic.open_float()
-	end, opts)
+	end, getOpts("Open diagnostics"))
+
 	vim.keymap.set("n", "gr", function()
 		vim.diagnostic.goto_next()
-	end, opts)
+	end, getOpts("Go to next diagnostic"))
 	-- vim.keymap.set("n", "bgr", function() vim.diagnostic.goto_prev() end, opts) -- Disabled cause it interferes with vim
-	vim.keymap.set("n", "<C-.>", function()
+	--
+	vim.keymap.set("n", "ga", function()
 		vim.lsp.buf.code_action()
-	end, opts)
+	end, getOpts("View code actions"))
+
 	vim.keymap.set("n", "<leader>gm", function()
 		vim.lsp.buf.references()
-	end, opts)
+	end, getOpts("View references"))
+
 	vim.keymap.set("n", "ge", function()
 		vim.lsp.buf.rename()
-	end, opts)
-	vim.keymap.set("i", "<C-h>", function()
-		vim.lsp.buf.signature_help()
-	end, opts)
+	end, getOpts("Rename symbol"))
 end)
 
 require("mason").setup({})
