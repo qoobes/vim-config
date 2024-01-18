6a8dc3b256d308fd73cd7acbeff1322999855630
Ahmed Haracic <me@qoobes.dev>
2024-01-18 18:21:23 +0100

    Fix remaps

--- lua/qoobes/remap.lua
+++ lua/qoobes/remap.lua
@@ -12,6 +12,7 @@ vim.keymap.set("x", "<leader>p", '"_dP')
 
 vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("x", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+Y')
 
 vim.keymap.set("n", "<leader>d", '"_d')
