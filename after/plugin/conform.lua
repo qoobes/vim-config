215f29529597f46a0b305a07bb746ee714c992e9
Ahmed Haracic <me@qoobes.dev>
2024-01-18 18:21:37 +0100

    Disable conform

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,17 +1,36 @@
-require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		-- Conform will run multiple formatters sequentially
-		python = { "isort", "black" },
-		-- Use a sub-list to run only the first available formatter
-		javascript = { { "prettierd", "prettier" } },
-	},
-})
-
-require("conform").setup({
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 500,
-		lsp_fallback = true,
-	},
-})
+-- require("conform").setup({
+-- 	formatters_by_ft = {
+-- 		lua = { "stylua" },
+-- 		-- Conform will run multiple formatters sequentially
+-- 		python = { "isort", "black" },
+-- 		-- Use a sub-list to run only the first available formatter
+-- 		-- javascript = { { "prettierd", "prettier" } },
+-- 		javascript = { { "prettierd", "prettier" } },
+-- 		javascriptreact = { { "prettierd", "prettier" } },
+-- 		typescript = { { "prettierd", "prettier" } },
+-- 		typescriptreact = { { "prettierd", "prettier" } },
+-- 	},
+-- })
+--
+-- require("conform").setup({
+-- 	format_on_save = {
+-- 		-- These options will be passed to conform.format()
+-- 		async = true,
+-- 		lsp_fallback = false,
+-- 		timeout_ms = 5000,
+-- 	},
+-- })
+--
+-- vim.api.nvim_create_user_command("Format", function(args)
+-- 	local range = nil
+-- 	if args.count ~= -1 then
+-- 		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+-- 		range = {
+-- 			start = { args.line1, 0 },
+-- 			["end"] = { args.line2, end_line:len() },
+-- 		}
+-- 	end
+-- 	require("conform").format({ async = true, lsp_fallback = true, range = range })
+-- end, { range = true })
+--
+-- -- lua require("conform").format({ async = true, })
