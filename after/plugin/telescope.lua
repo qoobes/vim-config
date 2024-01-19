8cc4f505c74b27815c35beb030169b11cdd900da
Ahmed Haracic <me@qoobes.dev>
2024-01-19 19:01:13 +0100

    Make telescope look for all files by default

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -7,7 +7,7 @@ vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 -- 	":Telescope file_browser path=%:p:h select_buffer=true<CR>",
 -- 	{ noremap = true }
 -- )
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
