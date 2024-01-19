c5680ab43ad2611103aa725404017556ce1a2b24
Ahmed Haracic <me@qoobes.dev>
2024-01-19 04:20:42 +0100

    Trouble magic

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,1 +1,14 @@
 require("trouble").setup()
+
+local trouble = require("trouble.providers.telescope")
+
+local telescope = require("telescope")
+
+telescope.setup({
+	defaults = {
+		mappings = {
+			i = { ["†"] = trouble.open_with_trouble },
+			n = { ["†"] = trouble.open_with_trouble },
+		},
+	},
+})
