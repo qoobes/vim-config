d5dc063c4ff2269513a4861aac86cbbfe9a48cd4
Ahmed Haracic <me@qoobes.dev>
2024-01-20 19:21:25 +0100

    Archive prettier

--- after/plugin/prettier.lua
+++ after/plugin/prettier.lua
@@ -1,156 +1,156 @@
--- Utilities for creating configurations
-local util = require("formatter.util")
-
-local function prettierd(parser)
-	if not parser then
-		return {
-			exe = "prettier",
-			args = {
-				"--stdin-filepath",
-				util.escape_path(util.get_current_buffer_file_path()),
-			},
-			stdin = true,
-			try_node_modules = true,
-		}
-	end
-
-	return {
-		exe = "prettier",
-		args = {
-			"--stdin-filepath",
-			util.escape_path(util.get_current_buffer_file_path()),
-			"--parser",
-			parser,
-		},
-		stdin = true,
-		try_node_modules = true,
-	}
-end
-
--- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-require("formatter").setup({
-	-- Enable or disable logging
-	logging = true,
-	-- Set the log level
-	log_level = vim.log.levels.WARN,
-	-- All formatter configurations are opt-in
-	filetype = {
-		-- Formatter configurations for filetype "lua" go here
-		-- and will be executed in order
-		lua = {
-			-- "formatter.filetypes.lua" defines default configurations for the
-			-- "lua" filetype
-			require("formatter.filetypes.lua").stylua,
-
-			-- You can also define your own configuration
-			function()
-				-- Supports conditional formatting
-				if util.get_current_buffer_file_name() == "special.lua" then
-					return nil
-				end
-
-				-- Full specification of configurations is down below and in Vim help
-				-- files
-				return {
-					exe = "stylua",
-					args = {
-						"--search-parent-directories",
-						"--stdin-filepath",
-						util.escape_path(util.get_current_buffer_file_path()),
-						"--",
-						"-",
-					},
-					stdin = true,
-				}
-			end,
-		},
-
-		javascript = { prettierd },
-		typescript = { prettierd },
-		javascriptreact = { prettierd },
-		typescriptreact = { prettierd },
-		vue = { prettierd },
-		["javascript.jsx"] = { prettierd },
-		["typescript.tsx"] = { prettierd },
-		markdown = { prettierd },
-		css = { prettierd },
-		json = { prettierd },
-		jsonc = { prettierd },
-		scss = { prettierd },
-		less = { prettierd },
-		yaml = { prettierd },
-		graphql = { prettierd },
-		html = { prettierd },
-
-		-- Use the special "*" filetype for defining formatter configurations on
-		-- any filetype
-		["*"] = {
-			-- "formatter.filetypes.any" defines default configurations for any
-			-- filetype
-			require("formatter.filetypes.any").remove_trailing_whitespace,
-		},
-	},
-})
-
-local lspconfig = require("lspconfig")
-
-local function organize_imports()
-	local params = {
-		command = "_typescript.organizeImports",
-		arguments = { vim.api.nvim_buf_get_name(0) },
-		title = "",
-	}
-	vim.lsp.buf.execute_command(params)
-end
-
-local function filter(arr, fn)
-	if type(arr) ~= "table" then
-		return arr
-	end
-
-	local filtered = {}
-	for k, v in pairs(arr) do
-		if fn(v, k, arr) then
-			table.insert(filtered, v)
-		end
-	end
-
-	return filtered
-end
-
-local function filterReactDTS(value)
-	if value.uri then
-		return string.match(value.uri, "%.d.ts") == nil and string.match(value.uri, "node_modules") == nil
-	elseif value.targetUri then
-		return string.match(value.targetUri, "%.d.ts") == nil and string.match(value.targetUri, "node_modules") == nil
-	end
-
-	return true
-end
-
-lspconfig.tsserver.setup({
-	commands = {
-		OrganizeImports = {
-			organize_imports,
-			description = "Organize Imports",
-		},
-	},
-	handlers = {
-		["textDocument/definition"] = function(err, result, method, ...)
-			if vim.tbl_islist(result) and #result > 1 then
-				local filtered_result = filter(result, filterReactDTS)
-				return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
-			end
-
-			vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
-		end,
-	},
-})
-
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-augroup("__formatter__", { clear = true })
-autocmd("BufWritePost", {
-	group = "__formatter__",
-	command = ":FormatWrite",
-})
+-- -- Utilities for creating configurations
+-- local util = require("formatter.util")
+--
+-- local function prettierd(parser)
+-- 	if not parser then
+-- 		return {
+-- 			exe = "prettier",
+-- 			args = {
+-- 				"--stdin-filepath",
+-- 				util.escape_path(util.get_current_buffer_file_path()),
+-- 			},
+-- 			stdin = true,
+-- 			try_node_modules = true,
+-- 		}
+-- 	end
+--
+-- 	return {
+-- 		exe = "prettier",
+-- 		args = {
+-- 			"--stdin-filepath",
+-- 			util.escape_path(util.get_current_buffer_file_path()),
+-- 			"--parser",
+-- 			parser,
+-- 		},
+-- 		stdin = true,
+-- 		try_node_modules = true,
+-- 	}
+-- end
+--
+-- -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+-- require("formatter").setup({
+-- 	-- Enable or disable logging
+-- 	logging = true,
+-- 	-- Set the log level
+-- 	log_level = vim.log.levels.WARN,
+-- 	-- All formatter configurations are opt-in
+-- 	filetype = {
+-- 		-- Formatter configurations for filetype "lua" go here
+-- 		-- and will be executed in order
+-- 		lua = {
+-- 			-- "formatter.filetypes.lua" defines default configurations for the
+-- 			-- "lua" filetype
+-- 			require("formatter.filetypes.lua").stylua,
+--
+-- 			-- You can also define your own configuration
+-- 			function()
+-- 				-- Supports conditional formatting
+-- 				if util.get_current_buffer_file_name() == "special.lua" then
+-- 					return nil
+-- 				end
+--
+-- 				-- Full specification of configurations is down below and in Vim help
+-- 				-- files
+-- 				return {
+-- 					exe = "stylua",
+-- 					args = {
+-- 						"--search-parent-directories",
+-- 						"--stdin-filepath",
+-- 						util.escape_path(util.get_current_buffer_file_path()),
+-- 						"--",
+-- 						"-",
+-- 					},
+-- 					stdin = true,
+-- 				}
+-- 			end,
+-- 		},
+--
+-- 		javascript = { prettierd },
+-- 		typescript = { prettierd },
+-- 		javascriptreact = { prettierd },
+-- 		typescriptreact = { prettierd },
+-- 		vue = { prettierd },
+-- 		["javascript.jsx"] = { prettierd },
+-- 		["typescript.tsx"] = { prettierd },
+-- 		markdown = { prettierd },
+-- 		css = { prettierd },
+-- 		json = { prettierd },
+-- 		jsonc = { prettierd },
+-- 		scss = { prettierd },
+-- 		less = { prettierd },
+-- 		yaml = { prettierd },
+-- 		graphql = { prettierd },
+-- 		html = { prettierd },
+--
+-- 		-- Use the special "*" filetype for defining formatter configurations on
+-- 		-- any filetype
+-- 		["*"] = {
+-- 			-- "formatter.filetypes.any" defines default configurations for any
+-- 			-- filetype
+-- 			require("formatter.filetypes.any").remove_trailing_whitespace,
+-- 		},
+-- 	},
+-- })
+--
+-- local lspconfig = require("lspconfig")
+--
+-- local function organize_imports()
+-- 	local params = {
+-- 		command = "_typescript.organizeImports",
+-- 		arguments = { vim.api.nvim_buf_get_name(0) },
+-- 		title = "",
+-- 	}
+-- 	vim.lsp.buf.execute_command(params)
+-- end
+--
+-- local function filter(arr, fn)
+-- 	if type(arr) ~= "table" then
+-- 		return arr
+-- 	end
+--
+-- 	local filtered = {}
+-- 	for k, v in pairs(arr) do
+-- 		if fn(v, k, arr) then
+-- 			table.insert(filtered, v)
+-- 		end
+-- 	end
+--
+-- 	return filtered
+-- end
+--
+-- local function filterReactDTS(value)
+-- 	if value.uri then
+-- 		return string.match(value.uri, "%.d.ts") == nil and string.match(value.uri, "node_modules") == nil
+-- 	elseif value.targetUri then
+-- 		return string.match(value.targetUri, "%.d.ts") == nil and string.match(value.targetUri, "node_modules") == nil
+-- 	end
+--
+-- 	return true
+-- end
+--
+-- lspconfig.tsserver.setup({
+-- 	commands = {
+-- 		OrganizeImports = {
+-- 			organize_imports,
+-- 			description = "Organize Imports",
+-- 		},
+-- 	},
+-- 	handlers = {
+-- 		["textDocument/definition"] = function(err, result, method, ...)
+-- 			if vim.tbl_islist(result) and #result > 1 then
+-- 				local filtered_result = filter(result, filterReactDTS)
+-- 				return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
+-- 			end
+--
+-- 			vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
+-- 		end,
+-- 	},
+-- })
+--
+-- local augroup = vim.api.nvim_create_augroup
+-- local autocmd = vim.api.nvim_create_autocmd
+-- augroup("__formatter__", { clear = true })
+-- autocmd("BufWritePost", {
+-- 	group = "__formatter__",
+-- 	command = ":FormatWrite",
+-- })
