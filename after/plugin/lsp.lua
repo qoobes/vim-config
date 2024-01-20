5a278876fbf535a0df31d9a6826c1a3a935b5645
Ahmed Haracic <me@qoobes.dev>
2024-01-20 17:04:21 +0100

    Better formatting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -77,12 +77,19 @@ cmp.setup({
 		end,
 	},
 	formatting = {
+		fields = {
+			cmp.ItemField.Kind,
+			cmp.ItemField.Abbr,
+			cmp.ItemField.Menu,
+		},
+
 		format = lspkind.cmp_format({
-			mode = "symbol_text", -- show only symbol annotations
+			mode = "symbol", -- show only symbol annotations
 			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 			-- can also be a function to dynamically calculate max width such as
 			-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
 			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+			show_labelDetails = true,
 		}),
 	},
 	preselect = "item",
