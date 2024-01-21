8809b516435a7eff97f32f5fda61b485c9ff29f3
Ahmed Haracic <me@qoobes.dev>
2024-01-21 19:43:11 +0100

    Update treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,40 +1,40 @@
 require("nvim-treesitter.configs").setup({
-	autotag = {
-		enable = true,
-		enable_rename = true,
-		enable_close = true,
-		enable_close_on_slash = true,
-		filetypes = { "html", "xml", "javascript", "javascriptreact", "typescriptreact", "svelte", "vue" },
-		update_in_insert = true,
-	},
-	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "vimdoc", "javascript", "typescript", "tsx", "c", "lua", "vim", "vimdoc", "query" },
+  autotag = {
+    enable = true,
+    enable_rename = true,
+    enable_close = true,
+    enable_close_on_slash = true,
+    filetypes = { "html", "xml", "javascript", "javascriptreact", "typescriptreact", "svelte", "vue" },
+    update_in_insert = true,
+  },
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = { "vimdoc", "javascript", "typescript", "tsx", "c", "lua", "vim", "vimdoc", "query" },
 
-	-- Install parsers synchronously (only applied to `ensure_installed`)
-	sync_install = false,
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
 
-	-- Automatically install missing parsers when entering buffer
-	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	auto_install = true,
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
 
-	highlight = {
-		enable = true,
+  highlight = {
+    enable = true,
 
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
-		additional_vim_regex_highlighting = false,
-	},
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
 })
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	underline = true,
-	virtual_text = {
-		spacing = 5,
-		severity_limit = "Warning",
-	},
-	update_in_insert = true,
+  underline = true,
+  virtual_text = {
+    spacing = 5,
+    severity_limit = "Warning",
+  },
+  update_in_insert = true,
 })
 
 require("nvim-ts-autotag").setup()
