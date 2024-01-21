21f8fee664a008e28711af02e2283a1f9b3ebf34
Ahmed Haracic <me@qoobes.dev>
2024-01-21 19:43:19 +0100

    Add back some things from the prettier file

--- after/plugin/prettier.lua
+++ after/plugin/prettier.lua
@@ -92,65 +92,65 @@
 -- 	},
 -- })
 --
--- local lspconfig = require("lspconfig")
---
--- local function organize_imports()
--- 	local params = {
--- 		command = "_typescript.organizeImports",
--- 		arguments = { vim.api.nvim_buf_get_name(0) },
--- 		title = "",
--- 	}
--- 	vim.lsp.buf.execute_command(params)
--- end
---
--- local function filter(arr, fn)
--- 	if type(arr) ~= "table" then
--- 		return arr
--- 	end
---
--- 	local filtered = {}
--- 	for k, v in pairs(arr) do
--- 		if fn(v, k, arr) then
--- 			table.insert(filtered, v)
--- 		end
--- 	end
---
--- 	return filtered
--- end
---
--- local function filterReactDTS(value)
--- 	if value.uri then
--- 		return string.match(value.uri, "%.d.ts") == nil and string.match(value.uri, "node_modules") == nil
--- 	elseif value.targetUri then
--- 		return string.match(value.targetUri, "%.d.ts") == nil and string.match(value.targetUri, "node_modules") == nil
--- 	end
---
--- 	return true
--- end
---
--- lspconfig.tsserver.setup({
--- 	commands = {
--- 		OrganizeImports = {
--- 			organize_imports,
--- 			description = "Organize Imports",
--- 		},
--- 	},
--- 	handlers = {
--- 		["textDocument/definition"] = function(err, result, method, ...)
--- 			if vim.tbl_islist(result) and #result > 1 then
--- 				local filtered_result = filter(result, filterReactDTS)
--- 				return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
--- 			end
---
--- 			vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
--- 		end,
--- 	},
--- })
---
--- local augroup = vim.api.nvim_create_augroup
--- local autocmd = vim.api.nvim_create_autocmd
--- augroup("__formatter__", { clear = true })
--- autocmd("BufWritePost", {
--- 	group = "__formatter__",
--- 	command = ":FormatWrite",
--- })
+local lspconfig = require("lspconfig")
+
+local function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = { vim.api.nvim_buf_get_name(0) },
+    title = "",
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
+local function filter(arr, fn)
+  if type(arr) ~= "table" then
+    return arr
+  end
+
+  local filtered = {}
+  for k, v in pairs(arr) do
+    if fn(v, k, arr) then
+      table.insert(filtered, v)
+    end
+  end
+
+  return filtered
+end
+
+local function filterReactDTS(value)
+  if value.uri then
+    return string.match(value.uri, "%.d.ts") == nil and string.match(value.uri, "node_modules") == nil
+  elseif value.targetUri then
+    return string.match(value.targetUri, "%.d.ts") == nil and string.match(value.targetUri, "node_modules") == nil
+  end
+
+  return true
+end
+
+lspconfig.tsserver.setup({
+  commands = {
+    OrganizeImports = {
+      organize_imports,
+      description = "Organize Imports",
+    },
+  },
+  handlers = {
+    ["textDocument/definition"] = function(err, result, method, ...)
+      if vim.tbl_islist(result) and #result > 1 then
+        local filtered_result = filter(result, filterReactDTS)
+        return vim.lsp.handlers["textDocument/definition"](err, filtered_result, method, ...)
+      end
+
+      vim.lsp.handlers["textDocument/definition"](err, result, method, ...)
+    end,
+  },
+})
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+augroup("imports", { clear = true })
+autocmd("BufWritePre", {
+  group = "imports",
+  command = ":OrganizeImports",
+})
