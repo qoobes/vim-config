4d0b02e482e2d81563325c4e5bc2f43705b95f2e
Ahmed Haracic <me@qoobes.dev>
2024-01-21 19:43:31 +0100

    Add null-ls back in...

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,36 +1,35 @@
--- WARNING: null-ls has been deprecated / archived, I won't rely on it
---
--- local null_ls = require("null-ls")
---
--- local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
--- local event = "BufWritePre" -- or "BufWritePost"
--- local async = event == "BufWritePost"
---
--- null_ls.setup({
--- 	on_attach = function(client, bufnr)
--- 		print("onattaching lsp")
--- 		if client.supports_method("textDocument/formatting") then
--- 			vim.keymap.set("n", "<Leader>f", function()
--- 				vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
--- 			end, { buffer = bufnr, desc = "[lsp] format" })
---
--- 			-- format on save
--- 			vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
--- 			vim.api.nvim_create_autocmd(event, {
--- 				buffer = bufnr,
--- 				group = group,
--- 				callback = function()
--- 					print("hi mom, I'm formatttttttting")
--- 					vim.lsp.buf.format({ bufnr = bufnr, async = async })
--- 				end,
--- 				desc = "[lsp] format on save",
--- 			})
--- 		end
---
--- 		if client.supports_method("textDocument/rangeFormatting") then
--- 			vim.keymap.set("x", "<Leader>f", function()
--- 				vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
--- 			end, { buffer = bufnr, desc = "[lsp] format" })
--- 		end
--- 	end,
--- })
+--- WARNING: null-ls has been deprecated / archived, but I use it until it breaks
+
+local null_ls = require("null-ls")
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+null_ls.setup({
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ async = false }) -- TODO: Switch to sync if it becomes an issue
+        end,
+      })
+    end
+  end,
+
+  sources = {
+    null_ls.builtins.formatting.prettierd.with({
+      env = {
+        PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.config/prettier/.prettierrc"),
+      },
+    }),
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.completion.spell,
+    null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.formatting.prismaFmt,
+    null_ls.builtins.formatting.remark,
+  },
+})
