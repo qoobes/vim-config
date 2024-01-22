672a11b674625cf5f91f8f91a3080a53cb225460
Ahmed Haracic <me@qoobes.dev>
2024-01-22 02:15:02 +0100

    Add some stuff to make diagnostics easier to live with oh god

---
+++ after/plugin/diagnostics.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_create_user_command("DiagnosticsToggleVirtualText", function()
+  local current_value = vim.diagnostic.config().virtual_text
+  if current_value then
+    vim.diagnostic.config({ virtual_text = false })
+  else
+    vim.diagnostic.config({ virtual_text = true })
+  end
+end, {})
+
+vim.keymap.set("n", "<leader>dt", ":DiagnosticsToggleVirtualText<CR>", { silent = true, noremap = true })
