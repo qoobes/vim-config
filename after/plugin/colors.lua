d89f80ff927c88ee8350d25dfe5c46de820d2c4d
Ahmed Haracic <me@qoobes.dev>
2024-01-26 02:04:55 +0100

    Use new themee

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,9 @@
 function ColorMyPencils(color)
-	color = color or "vscode"
+  color = color or "catppuccin-mocha"
 
-	vim.cmd.colorscheme(color)
-	vim.api.nvim_set_hl(1, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.cmd.colorscheme(color)
+  vim.api.nvim_set_hl(1, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
