b5cfe15fca3cdec8ac04506ec485b11d54a88a28
Ahmed Haracic <me@qoobes.dev>
2024-01-26 02:04:07 +0100

    set up fterm, even though it's not the best

--- after/plugin/terminal.lua
+++ after/plugin/terminal.lua
@@ -1,4 +1,15 @@
-require("toggleterm").setup({
+-- require("toggleterm").setup({
 
-	open_mapping = [[<c-\>]],
+-- 	open_mapping = [[<c-\>]],
+-- })
+
+require("FTerm").setup({
+  border = "double",
+  dimensions = {
+    height = 0.9,
+    width = 0.9,
+  },
 })
+
+vim.keymap.set("n", "<c-\\>", '<CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set("t", "<c-\\>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
