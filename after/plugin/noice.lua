d0b44a41cd6658b5f3228415c305e46ddb3365ee
Ahmed Haracic <me@qoobes.dev>
2024-01-26 02:04:43 +0100

    Drastically improve noice config

--- after/plugin/noice.lua
+++ after/plugin/noice.lua
@@ -9,10 +9,36 @@ require("noice").setup({
   },
   -- you can enable a preset for easier configuration
   presets = {
-    bottom_search = true, -- use a classic bottom cmdline for search
-    command_palette = true, -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false, -- add a border to hover docs and signature help
+    bottom_search = true,        -- use a classic bottom cmdline for search
+    command_palette = true,      -- position the cmdline and popupmenu together
+    long_message_to_split = false, -- long messages will be sent to a split
+    inc_rename = false,          -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false,      -- add a border to hover docs and signature help
   },
+  views = {
+    cmdline_popup = {
+      border = {
+        rounded = true,
+        padding = { 0, 1 },
+      },
+      filter_options = {},
+      win_options = {
+        winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+      },
+    },
+  },
+  routes = {
+    {
+      filter = {
+        event = "msg_show",
+        kind = "search_count",
+      },
+      opts = { skip = true },
+    },
+  },
+})
+
+vim.keymap.set("n", "<leader>nt", "<cmd>Noice telescope<cr>", {
+  noremap = true,
+  silent = true,
 })
