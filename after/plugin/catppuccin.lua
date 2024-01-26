6b821cd73e9306a38e8fc0eaff99575bb7fd4758
Ahmed Haracic <me@qoobes.dev>
2024-01-26 02:01:43 +0100

    Add catpuccin

---
+++ after/plugin/catppuccin.lua
@@ -0,0 +1,19 @@
+require("catppuccin").setup({
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		nvimtree = true,
+		treesitter = true,
+		notify = true,
+		noice = true,
+		mini = {
+			enabled = true,
+			indentscope_color = "",
+		},
+		telescope = {
+			enabled = true,
+			style = "nvchad",
+		},
+		which_key = true,
+	},
+})
