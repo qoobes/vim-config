5710f3d8b3b0a945ad93560b5ad14f483a6b1e91
Ahmed Haracic <me@qoobes.dev>
2024-01-29 01:43:49 +0100

    Update resesion

--- after/plugin/resession.lua
+++ after/plugin/resession.lua
@@ -18,9 +18,10 @@ resession.setup({
 
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
-    -- Only load the session if nvim was started with no args
-    -- Save these to a different directory, so our manual sessions don't get polluted
-    resession.load(vim.fn.getcwd(), { dir = "dirsession", silence_errors = true })
+    if vim.fn.argc(-1) == 0 then
+      print("cwd", vim.fn.getcwd())
+      resession.load(vim.fn.getcwd(), { dir = "dirsession", silence_errors = true })
+    end
   end,
 })
 vim.api.nvim_create_autocmd("VimLeavePre", {
@@ -39,6 +40,6 @@ vim.api.nvim_create_autocmd("VimLeavePre", {
 -- })
 --
 -- -- Resession does NOTHING automagically, so we have to set up some keymaps
--- vim.keymap.set("n", "<leader>ss", resession.save, opts("Save session"))
--- vim.keymap.set("n", "<leader>sl", resession.load, opts("Load session"))
--- vim.keymap.set("n", "<leader>sd", resession.delete, opts("Delete session"))
+vim.keymap.set("n", "<leader>ss", resession.save, opts("Save session"))
+vim.keymap.set("n", "<leader>sl", resession.load, opts("Load session"))
+vim.keymap.set("n", "<leader>sd", resession.delete, opts("Delete session"))
