2602eee015e3566abe595ef96f5a6c7a0fc252e9
Ahmed Haracic <me@qoobes.dev>
2024-02-16 14:26:44 +0100

    feat: add back toggleterm

--- after/plugin/terminal.lua
+++ after/plugin/terminal.lua
@@ -1,15 +1,17 @@
--- require("toggleterm").setup({
-
--- 	open_mapping = [[<c-\>]],
--- })
-
-require("FTerm").setup({
-  border = "double",
-  dimensions = {
-    height = 0.9,
-    width = 0.9,
-  },
+require("toggleterm").setup({
+  open_mapping = [[<c-\>]],
+  size = 20,
+  shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+  auto_scroll = true,    -- automatically scroll to the bottom on terminal output
 })
 
-vim.keymap.set("n", "<c-\\>", '<CMD>lua require("FTerm").toggle()<CR>')
-vim.keymap.set("t", "<c-\\>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+-- require("FTerm").setup({
+--   border = "double",
+--   dimensions = {
+--     height = 0.9,
+--     width = 0.9,
+--   },
+-- })
+--
+-- vim.keymap.set("n", "<c-\\>", '<CMD>lua require("FTerm").toggle()<CR>')
+-- vim.keymap.set("t", "<c-\\>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
